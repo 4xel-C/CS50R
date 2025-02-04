7ae89a82a7828c0bc95a950efe4efd06879a5394
4xel-C <axel-cano@live.fr>
2025-02-04 19:19:19 +0100

    Adding lecture source code

---
+++ lecture1/firstLesson.R
@@ -0,0 +1,30 @@
+# Assigning a string into a variable
+# Input the user for his name
+name <- readline("What's your name ? ")
+
+# Concatenating with paste or paste0 (no separator)
+greeting <- paste0("Hello, ", name)
+greeting <- paste("Hello, ", name, sep="")
+greeting <- paste("Hello,", name)
+
+print(greeting)
+
+# Console commands
+
+# Change working directory
+setwd(PATH) 
+
+# current directory
+getwd()
+
+# Change folder to lecture 1 by recreating the PATH (current directory + /newfolder)
+setwd(paste0(getwd(), "/lecture1"))
+
+# Create File in cwd (current working directory)
+file.create("filename")
+
+# get all the object in our environment
+ls()
+
+# Clean all the object from the environment
+rm(list = ls())
