f4731a9aad183d7df42a052d41feadcd5ec74e5c
4xel-C <axel-cano@live.fr>
2025-02-14 14:54:37 +0100

    Adding coloration for points over the limit in 'anita'

--- visualizing_data/lecture5/anita/anita1.R
+++ visualizing_data/lecture5/anita/anita1.R
@@ -4,15 +4,19 @@
 
 load("anita.RData")
 
+anita$color <- ifelse(anita$wind > 65, "red", "green") 
+
 ggplot(anita, aes(x = timestamp, y = wind)) +
   geom_line(
     linetype = 2,
     linewidth = 1
   ) +  # The order of the geom call influence if the layer is on front or in the back of another one
   geom_point(
-    color = "deepskyblue4",
+    aes(color = color),  # Add a color depending of the category color of the data points
+    show.legend = FALSE, # Hide the corresponding legend
     size = 4
   ) +
+  scale_color_identity() +  # Change the scale "color" to match the color in the dataframe
   geom_hline( # Add an horizontal line layer
     linetype = 1,
     yintercept = 65   # interception of the y axis
@@ -25,3 +29,4 @@ ggplot(anita, aes(x = timestamp, y = wind)) +
   theme_classic()
 
 anita
+
