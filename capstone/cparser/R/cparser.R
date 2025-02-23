220c1cfbf64dd8d556eb15c76ae7113b6c91f0ce
Axel Cano <axel-cano@live.fr>
2025-02-23 10:01:06 +0100

    Finalizing the package functions and testing

--- capstone/cparser/R/cparser.R
+++ capstone/cparser/R/cparser.R
@@ -3,28 +3,29 @@ parse_integer <- function(vec){
 }
 
 
-# -----------------------------------------------------------------To implement!!!
-# parse_float <- function(vec){
-#     parsed <- sapply(vec, function(x) {
+parse_float <- function(vec){
+    parsed <- sapply(vec, function(x) {
 
-#     })
-#     match <- regexpr("\\d+(?:[.,]\\d+)?", vec)
-#     parsed <- regmatches(vec, match)
+        # RegEx for floats
+        match <- regexpr("\\d+(?:[.,]\\d+)?", x)
+        num <- regmatches(x, match)
 
-#     if (length(parsed) == 0) {
-#         return(NA)
-#     }
-    
-#     # transform the extraction into a float after transforming the comma into a point
-#     parsed <- as.numeric(sub(",", ".", parsed))
+        if (length(num) == 0) {
+            return(NA)
+        }
 
-#     parsed <- ifelse(vec != )
+        return(num)
+    })
+   
+    # transform the extraction into a float after transforming the comma into a point
+    parsed <- as.numeric(sub(",", ".", parsed))
 
-#     return(parsed)
-# }
+
+    return(parsed)
+}
 
 check_valid_formula <- function(formula){
-    pattern <- "^([A-Z][a-z]*[1-9]*)+$"
+    pattern <- "^([A-Z][a-z]?[1-9]*)+$"
 
     if (grepl(pattern, formula)) {
         return(TRUE)
@@ -33,79 +34,76 @@ check_valid_formula <- function(formula){
     }
 }
 
+
 # Return a named list of each atom mapped with their number
 parse_formula <- function(formula) {
-    if (!check_valid_formula){
-        warning("x is not a valid formula")
+    if (!check_valid_formula(formula)){
+        warning(paste(formula, "is not a valid formula"))
         return(NA)
     }
 
     # get all the occurencies of each atoms
-    matches <- gregexpr("([A-Z][a-z]*)([0-9]*)", formula)
-
+    matches <- gregexpr("([A-Z][a-z]?)([0-9]*)", formula)
+    
     # Extract separatly all atoms and their number
     elements <- regmatches(formula, matches)[[1]]
-
+    
     # Initialize the named list "atoms"
     atoms <- c()
 
     for (element in elements) {
 
         # Get the symbol
-        symbole <- gsub("[0-9]", "", element)
-        
+        symbol <- gsub("[0-9]", "", element)
+     
         # Get the number
         quantity <- as.integer(gsub("[^0-9]", "", element))
-
+        
         if (is.na(quantity) || quantity == 0) {
-            quantity <- 1 
+            quantity <- 1
         }
         
         # Add the quantities
-        atomes[symbole] <- atomes[symbole] + quantity
+        if (!is.null(atoms[symbol]) && !is.na(atoms[symbol])) {
+            atoms[symbol] <- atoms[symbol] + quantity
+        } else {
+            atoms[symbol] <- quantity
+        }
     }
-
     return(atoms)
 }
+parse_formula("H2O")
+
 
 compare_formula <- function(formula1, formula2){
     atoms1 <- parse_formula(formula1)
     atoms2 <- parse_formula(formula2)
 
-    if (atoms1 == atoms2) {
+    if (all(is.na(atoms1)) || all(is.na(atoms2))) {
+        return(FALSE)
+    }
+
+    if (all(atoms1 == atoms2)) {
         return(TRUE)
     }
     return(FALSE)
 }
 
-is_peroxydes <- function(smiles){
-    grepl("OO", smiles)
-}
 
 estimate_explosivity <- function(formula) {
     atoms <- parse_formula(formula)
 
+    if(atoms["O"] == 0 || atoms["C"] == 0 || is.na(atoms["O"]) || is.na(atoms["C"])){
+        warning("There is no carbon nor oxygen")
+        return(NA)
+    }
+
     # get the number of Oxygen
-    O <- atoms["O"]
+    O <- atoms[["O"]]
 
     # get the number of Carbons
-    C <- atoms["C"]
+    C <- atoms[["C"]]
 
-    return(ifelse(C > 0, O / C, NA))
+
+    return(O / C)
 }
-
-
-
-
-
-
-match <- regexpr("\\d+(?:[.,]\\d+)?", c("xcwv", "xcwv", "xwcv", "3"))
-parsed <- regmatches(c("xcwv", "xcwv", "xwcv", "3"), match)
-as.numeric(parsed)
-
-
-match <- regexpr("\\d+(?:[.,]\\d+)?", "test")
-regmatches("test", match)
-
-# transform the extraction into a float after transforming the comma into a point
-parsed <- as.numeric(sub(",", ".", parsed))
